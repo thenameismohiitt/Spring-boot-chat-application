219152f10f67eeab42f17d2d623e843caf90ba88
thenameismohiitt <mohitchandani50@yahoo.in>
2021-01-24 16:26:28 +0530

    WebChat Application

---
+++ src/test/java/com/websocketdemo/websocketdemo/WebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocketdemo.websocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
